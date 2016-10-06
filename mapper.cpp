5b2e2684d18f2e40a00299fb2a336a7b2bc04f00
gypleon <gyp_leon@hotmail.com>
2016-10-06 10:44:27 +0800

    backup

---
+++ mapper.cpp
@@ -0,0 +1,23 @@
+#include <thread>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int createthreads(vector<thread> &threads, int num){
+    for(int i=0; i<num; i++){
+    
+    }
+
+}
+
+int main(){
+    
+    vector<thread> threads;
+    createthreads(threads);
+    t1.join();
+    t2.join();
+    return 0;
+}
+
